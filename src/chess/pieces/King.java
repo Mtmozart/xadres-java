335749b78d106b408fe5d17d377f85c5a6adf70d
matheus mozart <mmsnborges@gmail.com>
2023-12-04 18:55:06 -0400

    corrections for the king's move

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -18,14 +18,14 @@ public class King extends ChessPiece {
 	}
 
 	private boolean canMove(Position position) {
-
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
-		return p == null || p.getColor() != p.getColor();
+		return p == null || p.getColor() != getColor();
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
 		Position p = new Position(0, 0);
 
 		// above
