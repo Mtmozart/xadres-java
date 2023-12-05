fe90474d6043c7d5d8bec5e1c373722dc509fa9e
matheus mozart <mmsnborges@gmail.com>
2023-12-04 23:25:48 -0400

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -71,9 +71,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrent());
-		if(chessMatch.getCheck()) {
-			System.out.println("Check");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrent());
+			if(chessMatch.getCheck()) {
+				System.out.println("Check");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE");
+			System.out.println("The winner is: " + chessMatch.getCurrent());
 		}
 	}
 	
